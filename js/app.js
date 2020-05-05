fcbf9205fc6cae8cafa5f782825df6b0e30b6dc2
Alex <aleksey.egorov@teko.biz>
2020-05-05 15:30:50 +0300

    add aurorizate

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
-const menuButton = document.querySelector('#menu');
-const actionButton = document.querySelector('#menu-action');
-const contentDiv = document.querySelector('.content-div');
+const menuButton = document.querySelector('#menu'), actionButton = document.querySelector('#menu-action'),
+    contentDiv = document.querySelector('.content-div'), buttonLogin = document.querySelector('#login'),
+    autorizationDiv = document.querySelector('.autorization'), row = document.querySelector('.row');
 
 menuButton.addEventListener('click', () => {
     const divMenu = document.createElement('div');
-    if ( contentDiv.innerHTML !== '' )contentDiv.innerHTML='';
+    if (contentDiv.innerHTML !== '') contentDiv.innerHTML = '';
 
     divMenu.innerHTML = 'Элемент создан';
-    divMenu.style.color= 'white';
-    const buttonRemove =  document.createElement('button');
+    divMenu.style.color = 'white';
+    const buttonRemove = document.createElement('button');
     buttonRemove.innerHTML = 'X';
 
 
@@ -16,25 +16,31 @@ menuButton.addEventListener('click', () => {
     divMenu.appendChild(buttonRemove);
     console.log(divMenu);
     buttonRemove.addEventListener('click', () => {
-        divMenu.innerHTML='';
+        divMenu.innerHTML = '';
     })
 });
 
-actionButton.addEventListener('click',() => {
+actionButton.addEventListener('click', () => {
     const divMenu = document.createElement('div');
-    if ( contentDiv.innerHTML !== '' )contentDiv.innerHTML='';
-    contentDiv.innerHTML='';
+    if (contentDiv.innerHTML !== '') contentDiv.innerHTML = '';
+    contentDiv.innerHTML = '';
 
-    divMenu.style.color= 'white';
-    const buttonRemove =  document.createElement('button');
+    divMenu.style.color = 'white';
+    const buttonRemove = document.createElement('button');
     buttonRemove.innerHTML = 'X';
 
     divMenu.innerHTML = 'Акция';
     contentDiv.appendChild(divMenu)
     divMenu.appendChild(buttonRemove);
     buttonRemove.addEventListener('click', () => {
-       divMenu.innerHTML='';
+        divMenu.innerHTML = '';
 
     })
 });
 
+buttonLogin.addEventListener('click', () => {
+    alert('Привет, пользователь');
+    autorizationDiv.innerHTML='';
+    row.style.display = '';
+
+});
